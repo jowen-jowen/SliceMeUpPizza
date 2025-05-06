d7676cb8a3ba5aaca1f1665b82f45c130293a6e0
TEOPE, CEZAR DOMINIC T. <teopecezar6@gmail.com>
2025-05-06 18:02:53 +0800

    finished the receipt

--- app/src/main/java/com/example/slicemeuppizza/Reciept.java
+++ app/src/main/java/com/example/slicemeuppizza/Reciept.java
@@ -1,7 +1,9 @@
 package com.example.slicemeuppizza;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Reciept extends AppCompatActivity {
     Button back;
+    TextView pizza, crust, toppings, price;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +24,26 @@ public class Reciept extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        back = findViewById(R.id.btnMakePurchase);
+
+        pizza = findViewById(R.id.pizza);
+        crust = findViewById(R.id.pizzaCrust);
+        toppings = findViewById(R.id.toppings);
+        price = findViewById(R.id.pizzaPrice);
+
+        Intent fetch= getIntent();
+        String receivedPizza = fetch.getStringExtra("selectedPizza");
+        String receivedCrust = fetch.getStringExtra("selectedCrust");
+        String receivedToppings = fetch.getStringExtra("selectedToppings");
+        String receivedPrice = fetch.getStringExtra("totalPrice");
+
+        price.setText(receivedPizza);
+        crust.setText(receivedCrust);
+        toppings.setText(receivedToppings);
+        price.setText(receivedPrice);
+
+
+
     }
 }
